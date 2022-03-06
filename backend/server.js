482173dc031a23ec38bdbd6664d2835fa5215dd0
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-06 20:30:24 +0300

    connection push

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ const path = require("path");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 
