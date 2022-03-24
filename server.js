fad2070dbc87a43bc1b0c2b4c2689fc4b515b1d6
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-24 22:29:08 +0300

    I removed lower part from file path

--- server.js
+++ server.js
@@ -18,9 +18,6 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'))
-})
 
 /*
 app.use(express.static(path.join(__dirname, './frontend/build')));
